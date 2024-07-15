cfc580f5f21e0f79556138a1f37756bd0a208a1a
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-07-15 10:46:02 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import React from "react";
 
 class App extends React.Component 
 {
-
-  constructor()
-  {
-    super()
-    this.nome = "Pedro Henrique"
-  }
-
   render()
   {
     return (
-      <div>
-        <h1>Hello World!!</h1>
-      
-        <h2>{this.nome}</h2>
-      </div>
+      <section id="section-navbar">
+
+        <div id="image-title-navbar">
+          <img id="image-navbar" src="saturn-icon.png" alt="Icone do planeta Saturno"/>
+          <h2 id="title-navbar">Space Flight News</h2>
+        </div>
+
+        <ul id="links-navbar">
+          <li>Home</li>
+          <li>Trending</li>
+          <li>Categories</li>
+          <li>About us</li>
+        </ul>
+      </section>
     );
   }
 
